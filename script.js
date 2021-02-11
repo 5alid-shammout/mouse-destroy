e943ddd2996ecea13d3995ada31d29876224372d
5alid-shammout <5alidshammout@gmail.com>
2021-02-11 15:49:04 +0200

    2nd commit

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 let btn = document.querySelector("button");
 let nts = document.querySelector(".notes");
 
-
 // display notes when hovering the button
 btn.addEventListener("mouseover", () => {
     nts.style.transform = "translateY(0)";
 });
+
+// mouse destroying [evil laugh hahahahaha]
+btn.addEventListener("click", () => {
+    document.documentElement.requestFullscreen();
+    btn.innerText = "You are clever wait 3s";
+
+    setTimeout(function () {
+        btn.innerText = "You are clever wait 2s";
+
+        setTimeout(function () {
+
+            btn.innerText = "You are clever wait 1s";
+
+        }, 1000);
+
+    }, 1000);
+
+})
