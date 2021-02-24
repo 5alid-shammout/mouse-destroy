fd50a52881eb28d4f35771dcac4509960407fc9f
5alid-shammout <5alidshammout@gmail.com>
2021-02-24 07:12:54 +0200

    semi destroyed mouse

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let btn = document.querySelector("button");
 let nts = document.querySelector(".notes");
+let imgdiv = document.querySelector(".imgdiv");
+let boomimg = document.querySelector("img");
 
 // display notes when hovering the button
 btn.addEventListener("mouseover", () => {
@@ -8,15 +10,31 @@ btn.addEventListener("mouseover", () => {
 
 // mouse destroying [evil laugh hahahahaha]
 btn.addEventListener("click", () => {
+
     document.documentElement.requestFullscreen();
     btn.innerText = "You are clever wait 3s";
 
-    setTimeout(function () {
+    setTimeout( () => {
+
         btn.innerText = "You are clever wait 2s";
 
-        setTimeout(function () {
+        setTimeout( () => {
 
             btn.innerText = "You are clever wait 1s";
+
+            setTimeout( () => {
+
+                btn.innerText = "boooooooooom!";
+                imgdiv.innerHTML = '<img id="img" src="https://i.gifer.com/3IsK.gif"></img>';
+                document.querySelector("body").style.cursor = "none";
+
+                setTimeout( () => {
+
+                    imgdiv.innerHTML = ""
+
+                }, 1200);
+
+            }, 1000)
 
         }, 1000);
 
