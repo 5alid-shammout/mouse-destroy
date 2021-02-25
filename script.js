c17ec563f04b94dc226808241fea3bc890b5341f
Szymon Adamiak <adamiak.szymon@gmail.com>
2021-02-25 14:03:50 +0100

    Align image with mouse

--- script.js
+++ script.js
@@ -5,43 +5,40 @@ let boomimg = document.querySelector("img");
 
 // display notes when hovering the button
 btn.addEventListener("mouseover", () => {
-    nts.style.transform = "translateY(0)";
+  nts.style.transform = "translateY(0)";
+});
+
+const body = document.body;
+const el = document.querySelector("#img");
+
+body.addEventListener("mousemove", (e) => {
+  el.style.left = e.pageX - 100 + "px";
+  el.style.top = e.pageY - 170 + "px";
 });
 
 // mouse destroying [evil laugh hahahahaha]
-btn.addEventListener("click", () => {
+btn.addEventListener("click", (e) => {
+  document.documentElement.requestFullscreen();
+  btn.innerText = "You are clever wait 3s";
 
-    document.documentElement.requestFullscreen();
-    btn.innerText = "You are clever wait 3s";
+  setTimeout(() => {
+    btn.innerText = "You are clever wait 2s";
 
-    setTimeout( () => {
+    setTimeout(() => {
+      btn.innerText = "You are clever wait 1s";
 
-        btn.innerText = "You are clever wait 2s";
+      setTimeout(() => {
+        btn.innerText = "boooooooooom!";
+        el.style.opacity = 1;
 
-        setTimeout( () => {
+        document.querySelector("body").style.cursor = "none";
+        btn.style.cursor = "none";
+        nts.style.transform = "translateY(2.5vh)";
 
-            btn.innerText = "You are clever wait 1s";
-
-            setTimeout( () => {
-
-                btn.innerText = "boooooooooom!";
-                imgdiv.innerHTML = '<img id="img" src="https://i.gifer.com/3IsK.gif"></img>';
-                
-                document.querySelector("body").style.cursor = "none";
-                btn.style.cursor = "none";
-                nts.style.transform = "translateY(2.5vh)";
-                
-
-                setTimeout( () => {
-
-                    imgdiv.innerHTML = ""
-
-                }, 1200);
-
-            }, 1000)
-
-        }, 1000);
-
+        setTimeout(() => {
+          el.style.opacity = 0;
+        }, 1200);
+      }, 1000);
     }, 1000);
-
+  }, 1000);
 });
